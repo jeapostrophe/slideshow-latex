ea6b173165b305cee8464749646b89d1e29e6a53
Jay McCarthy <jay.mccarthy@gmail.com>
2013-08-09 08:30:38 -0600

    meta

--- info.rkt
+++ info.rkt
@@ -1,2 +1,5 @@
 #lang setup/infotab
+(define collection 'multi)
 (define setup-collects '("slideshow/latex"))
+(define deps '("base" "draw-lib" "gui-lib" "slideshow-lib"))
+(define build-deps '("racket-doc" "scribble-lib" "slideshow-doc"))
