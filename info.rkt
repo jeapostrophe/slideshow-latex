9520ae68ae27a425ba2ebb8182f9ff73d673eff5
Leif Andersen <leif@leifandersen.net>
2014-08-06 18:22:35 -0400

    Fix dependency issues.

--- info.rkt
+++ info.rkt
@@ -2,4 +2,4 @@
 (define collection 'multi)
 (define setup-collects '("slideshow/latex"))
 (define deps '("base" "draw-lib" "gui-lib" "slideshow-lib"))
-(define build-deps '("racket-doc" "scribble-lib" "slideshow-doc"))
+(define build-deps '("racket-doc" "scribble-lib" "slideshow-doc" "planet-doc"))
